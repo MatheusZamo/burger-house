d2a437b6a6448f95f7961bed8339fb00a92cdc53
MatheusZamo <19matheushenrique03@gmail.com>
2024-12-01 14:25:44 -0300

    Contact component remade and finalized

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,12 @@
 const Contact = () => {
   return (
     <div id="contact">
-      <h1>BOOK YOUR TABLE</h1>
+      <h1>RESERVE SUA MESA</h1>
       <form>
         <input type="text" placeholder="Nome Completo" />
         <input type="email" placeholder="Email" />
         <textarea placeholder="Escreve sua mensagem..."></textarea>
-        <input type="submit" placeholder="BOOK" />
+        <button>ENVIAR</button>
       </form>
     </div>
   )
